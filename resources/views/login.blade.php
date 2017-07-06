9f431862a598ced1df4481ed94ba8a9a18aadbf0
jon-guiriba01 <jonguiriba01@gmail.com>
2017-07-06 18:14:17 +0800

    init

---
+++ resources/views/login.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		@include('master')
+        <title>Laravel</title>
+    </head>
+    <body>
+       <div class="container">
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4 text-center">
+			<div class="search-box">
+				<div class="caption">
+					<h3>Advance Password Validation</h3>
+					<p>Find to All</p>
+				</div>
+				<form action="" class="loginForm">
+					<div class="input-group">
+						<input type="text" id="name" class="form-control" placeholder="Full Name">
+						<input type="password" id="paw" class="form-control" placeholder="Password">
+						<input type="submit" id="submit" class="form-control" value="Submit">
+					</div>
+				</form>
+			</div>
+		</div>
+		<div class="col-md-4">
+			<div class="aro-pswd_info">
+				<div id="pswd_info">
+					<h4>Password must be requirements</h4>
+					<ul>
+						<li id="letter" class="invalid">At least <strong>one letter</strong></li>
+						<li id="capital" class="invalid">At least <strong>one capital letter</strong></li>
+						<li id="number" class="invalid">At least <strong>one number</strong></li>
+						<li id="length" class="invalid">Be at least <strong>8 characters</strong></li>
+						<li id="space" class="invalid">be<strong> use [~,!,@,#,$,%,^,&,*,-,=,.,;,']</strong></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+    </body>
+</html>
