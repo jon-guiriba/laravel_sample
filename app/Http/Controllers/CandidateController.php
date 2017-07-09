306891d083669409689147a39ab275f2874492cb
jon-guiriba <jon.guiriba@gmail.com>
2017-07-09 22:02:31 +0800

    7

--- app/Http/Controllers/CandidateController.php
+++ app/Http/Controllers/CandidateController.php
@@ -6,6 +6,8 @@ use App\Models\Candidate;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\File;
+use Illuminate\Database\QueryException;
+
 class CandidateController extends Controller
 {
     /**
@@ -27,17 +29,20 @@ class CandidateController extends Controller
         $this->validate($request, [
             'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
+
         $image = Input::file('image');
 
         $candidate = new Candidate;
         $candidate->image = base64_encode(file_get_contents($image->getRealPath()));
         $candidate->mimeType = File::mimeType($image->getRealPath());
-        $candidate->name = "test";
-        $candidate->description = "testDescription";
-        $candidate->save();
-        
-        return back()
-            ->with('success','Image Uploaded successfully.');
+        $candidate->name = Input::get('name');
+        $candidate->description = Input::get('description');
 
+    try { 
+      $candidate->save();   
+    } catch(QueryException $ex){ 
+      return back();
+    }
+        return back(); 
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,58 +1,64 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="fluid-container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="row margin-top-25" style="height: 100px;">
-                 <h1 class="text-center">Most Influencial People 2018</h1>
-            </div>
+<div class="container-fluid">
+        <div class="row margin-top-25" style="height: 100px;">
+          <div class="col-md-12">
+            <h1 class="text-center">Most Influencial People 2018</h1>
+          </div>
+        </div>
             
-            <div id="candid-carousel" class="carousel slide col-md-8 col-md-offset-2" data-ride="carousel">
-              <!-- Indicators -->
+        <div class="row" >
+          <div class="col-md-12" >
+            <div id="candid-carousel" class="carousel slide col-md-8 col-md-offset-2" data-ride="carousel" style="height:600px;">
 
               <div class="carousel-inner" role="listbox">
-                @if ($candidates)
-                  @foreach($candidates as $candidate)
-                    @if ($loop->first)
-                      <div class="item active">
-                        <img class="first-slide" src="data:{{$candidate->mimeType}};charset=utf-8;base64,{{$candidate->image}}">
-                      </div>
-                    @else
-                      <div class="item">
-                        <img class="first-slide" src="data:{{$candidate->mimeType}};charset=utf-8;base64,{{$candidate->image}}">
-                      </div>
-                    @endif
+                  @if ($candidates)
+                    @foreach($candidates as $candidate)
+                      @if ($loop->first)
+                        <div class="item active">
+                          <img class="first-slide" src="data:{{$candidate->mimeType}};charset=utf-8;base64,{{$candidate->image}}">
+                            <div class="text-center">
+                              <span class="center-block">{{$candidate->name}}</span>
+                              <span class="center-block">{{$candidate->description}}</span>
+                            </div>
+                        </div>
+                      @else
+                        <div class="item">
+                          <img class="first-slide" src="data:{{$candidate->mimeType}};charset=utf-8;base64,{{$candidate->image}}">
+                            <div class="text-center">
+                              <span class="center-block">{{$candidate->name}}</span>
+                              <span class="center-block">{{$candidate->description}}</span>
+                            </div>
+                        </div>
+                      @endif
 
-                  @endforeach
-                @endif
-              </div>
-              <a class="left carousel-control" href="#candid-carousel" role="button" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-              </a>
-              <a class="right carousel-control" href="#candid-carousel" role="button" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-              </a>
-            </div><!-- /.carousel -->
-        </div>
-    </div>
-    <div class="row">
-        <span></span>
-    </div>
-    <div class="row">
-      <form action="{{ route('addCandidate') }}" enctype="multipart/form-data" method="POST">
-        {{ csrf_field() }}
-        <div class="row">
-          <div class="col-md-12">
-            <input type="file" name="image" />
-          </div>
-          <div class="col-md-12">
-            <button type="submit" class="btn btn-success">Upload</button>
+                    @endforeach
+                  @endif
+                </div>
+                <a class="left carousel-control" href="#candid-carousel" role="button" data-slide="prev">
+                  <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="right carousel-control" href="#candid-carousel" role="button" data-slide="next">
+                  <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+            </div>
           </div>
         </div>
-      </form>
+
+    <div class="row margin-top-40 text-center">
+      <div class="col-md-3 col-centered">
+          <form action="{{ route('addCandidate') }}" enctype="multipart/form-data" method="POST">
+          {{ csrf_field() }}
+            
+            <input class="center-block form-control" type="text" name="name" placeholder="Name"/>
+            <textarea class="center-block margin-top-25 form-control" name="description" rows="5" cols="30" placeholder="Description" style="resize: none;"></textarea>
+            <input class="margin-left-40 margin-top-25 " type="file" name="image" />
+            <button class="btn btn-success center-block margin-top-25" type="submit">Upload</button>
+          </form>
+        </div>
+      </div>
     </div>
-</div>
 @endsection
